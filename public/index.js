d72cd2ce1db1b314fb8777badce089bd1bb25c23
mikitosina1 <mikitosina@mail.ru>
2025-01-22 01:58:28 +0100

    fix using mongodb from ddev

--- public/index.js
+++ public/index.js
@@ -1,2 +1,14 @@
 const os = require('os');
-console.log(os);
+// console.log(os);
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb://mongodb:27017';
+const client = new MongoClient(uri);
+
+(async () => {
+	try {
+		await client.connect();
+		console.log('Connected to MongoDB!');
+	} finally {
+		await client.close();
+	}
+})();
